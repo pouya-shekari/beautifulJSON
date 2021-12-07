f0f3b27db9eefdec48135c03b15c94988b87689c
pouya-shekari <pshekari37@gmail.com>
2021-12-07 19:34:48 +0330

    first update

--- main.js
+++ main.js
@@ -2,4 +2,25 @@ const resultTag = document.getElementById("result");
 const textArea = document.getElementById("textarea");
 const submitBtn = document.getElementById("submit-btn");
 
-// TODO: Write your codes here
+function onToggleObj(e){
+    if(e.target.classList.contains("json-viewer-object-start")){
+        e.target.classList.toggle("json-viewer-object-close")
+    }
+}
+
+function JSONBeautifuler(data){
+    let rootElement = document.createElement('ul')
+    render(rootElement , data)
+    rootElement.addEventListener('click' , onToggleObj)
+    return rootElement
+}
+
+submitBtn.addEventListener("click",function(){
+    resultTag.innerHTML = null
+    try{
+        textArea.classList.remove('has-error')
+        resultTag.appendChild(JSONBeautifuler(JSON.parse(textArea.value)))
+    }catch (e) {
+        textArea.classList.add('has-error')
+    }
+})
