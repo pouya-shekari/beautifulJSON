3fe11a6db8b93ae61d0f9ad79a42887ad9fa36e5
Pedram <pedramnili@gmail.com>
2021-12-07 18:47:08 +0330

    test

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const textArea = document.getElementById("textarea");
 const submitBtn = document.getElementById("submit-btn");
 
 // TODO: Write your codes here
+
+submitBtn.addEventListener("click",() => {
+    console.log(textArea.value);
+})
+
