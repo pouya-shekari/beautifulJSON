87be2eec55a114dc1ef88912499d1a518f82d8bb
pouya-shekari <pshekari37@gmail.com>
2021-12-11 10:42:07 +0330

    final

--- main.js
+++ main.js
@@ -2,6 +2,42 @@ const resultTag = document.getElementById("result");
 const textArea = document.getElementById("textarea");
 const submitBtn = document.getElementById("submit-btn");
 
+
+function render(container, obj) {
+    container.innerHTML += `<span class="json-viewer-object-start">{</span>`
+    container.appendChild(createTreeDom(obj));
+    container.innerHTML += `<span class="json-viewer-object-end">}</span>`
+}
+
+function createTreeDom(obj) {
+    let ul = document.createElement("ul");
+    for (let key in obj) {
+        let li = document.createElement('li');
+        if (typeof obj[key] === 'object') {
+            let childrenUl = createTreeDom(obj[key]);
+
+            li.innerHTML=`<span class="json-viewer-object-start">"${key}" : {</span>`
+            li.appendChild(childrenUl);
+
+            li.innerHTML+=  `  <span class="json-viewer-object-end"> }</span>`
+
+        }else if(typeof obj[key] === 'string'){
+            li.innerHTML +=  `<span class="key">"${key}" :</span>
+                                   <span class="value-${typeof obj[key]}">"${obj[key]}"</span>`;
+
+        }else{
+            li.innerHTML +=  `<span class="key">"${key}" :</span>
+                                   <span class="value-${typeof obj[key]}"> ${obj[key]}</span>`;
+
+        }
+
+        ul.appendChild(li);
+    }
+
+    return ul;
+}
+
+
 function onToggleObj(e){
     if(e.target.classList.contains("json-viewer-object-start")){
         e.target.classList.toggle("json-viewer-object-close")
